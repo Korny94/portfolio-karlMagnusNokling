989c35780101bc8f83b4520c9943731c776e1d50
Korny94 <karlmagnusnokling@hotmail.no>
2023-06-02 02:42:36 +0200

    update

---
+++ index.js
@@ -0,0 +1,37 @@
+// const cardExam = document.querySelector("#cardExam");
+
+// const cardSquareEyes = document.querySelector("#cardSquareEyes");
+
+// const cardMuseum = document.querySelector("#cardMuseum");
+
+// const cardChat = document.querySelector("#cardChat");
+
+// const cardKeyGame = document.querySelector("#cardKeyGame");
+
+// const hello = "hello";
+
+// const gitExam = document.querySelector("#gitExam");
+
+// function hover(event, arg) {
+//   event.target.style.transform = "scale(1.03)";
+//   arg.style.opacity = "1";
+// }
+
+// cardExam.addEventListener("mouseover", function (event) {
+//   hover(event, gitExam);
+// });
+
+// function unhover(arg) {
+//   this.style.transform = "scale(1)";
+// }
+
+// // cardSquareEyes.addEventListener("mouseover", hover);
+// // cardMuseum.addEventListener("mouseover", hover);
+// // cardChat.addEventListener("mouseover", hover);
+// // cardKeyGame.addEventListener("mouseover", hover);
+
+// // cardExam.addEventListener("mouseout", unhover);
+// // cardSquareEyes.addEventListener("mouseout", unhover);
+// // cardMuseum.addEventListener("mouseout", unhover);
+// // cardChat.addEventListener("mouseout", unhover);
+// // cardKeyGame.addEventListener("mouseout", unhover);
