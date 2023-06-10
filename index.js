e1ecb154d5d8dc9274d47d31a408728dfc8c1f39
Korny94 <karlmagnusnokling@hotmail.no>
2023-06-10 19:03:25 +0200

    update

--- index.js
+++ index.js
@@ -77,3 +77,20 @@ cardKeyGame.addEventListener("click", (event) =>
 cardBionicReading.addEventListener("click", (event) =>
   redirectToWebsite(event, "https://bionicreading.netlify.app/")
 );
+
+const links = [
+  "https://project-exam-1-korny94.netlify.app/index.html",
+  "https://my-first-website-square-eyes.netlify.app/",
+  "https://semester-project-1-leonardo.netlify.app/",
+  "https://chatgpt-hee.netlify.app/",
+  "https://findkeygame.netlify.app/index.html",
+  "https://bionicreading.netlify.app/",
+];
+
+const randomLink = document.getElementById("random");
+
+randomLink.onclick = function () {
+  const randomIndex = Math.floor(Math.random() * links.length);
+  const selectedLink = links[randomIndex];
+  window.open(selectedLink, "_blank");
+};
