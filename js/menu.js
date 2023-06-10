7bfa6a83a535983d206c290278ebc7a71a3e4d11
Korny94 <karlmagnusnokling@hotmail.no>
2023-06-10 22:31:36 +0200

    update

--- js/menu.js
+++ js/menu.js
@@ -2,15 +2,42 @@ const hamMenu = document.querySelector("#hamMenu");
 const menuContainer = document.querySelector("#menuContainer");
 const cardContainer = document.querySelector("#cardContainer");
 
-let menuCount = 0;
+let menuOpen = false;
 
-hamMenu.onclick = function () {
-  menuCount++;
-  if (menuCount % 2 === 0) {
-    menuContainer.style.display = "none";
-    cardContainer.style.marginRight = "0";
+hamMenu.onclick = function (event) {
+  if (menuOpen) {
+    hideMenu();
   } else {
-    menuContainer.style.display = "flex";
-    cardContainer.style.marginRight = "20vw";
+    showMenu();
   }
 };
+
+function showMenu() {
+  menuContainer.style.display = "flex";
+  cardContainer.style.marginRight = "20vw";
+  menuOpen = true;
+
+  // Add event listener to the document to handle clicks outside of menuContainer
+  document.addEventListener("click", clickOutsideMenu);
+}
+
+function hideMenu() {
+  menuContainer.style.display = "none";
+  cardContainer.style.marginRight = "0";
+  menuOpen = false;
+
+  // Remove event listener from the document
+  document.removeEventListener("click", clickOutsideMenu);
+}
+
+function clickOutsideMenu(event) {
+  const targetElement = event.target;
+
+  // Check if the clicked element is outside the menuContainer and hamMenu
+  if (
+    !menuContainer.contains(targetElement) &&
+    !hamMenu.contains(targetElement)
+  ) {
+    hideMenu();
+  }
+}
